6bbef415a431f2d9186ba01cdb09bbd1d3e55311
guilopes2020 <guilopesdev@gmail.com>
2024-09-02 14:04:56 -0300

    feat: :sparkles: implementing-crud

---
+++ Pages/Premiums/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using crud_aspnet_core_mvc_balta.Data;
+using crud_aspnet_core_mvc_balta.Models;
+
+namespace crud_aspnet_core_mvc_balta.Pages.Premiums
+{
+    public class IndexModel : PageModel
+    {
+        private readonly crud_aspnet_core_mvc_balta.Data.ApplicationDbContext _context;
+
+        public IndexModel(crud_aspnet_core_mvc_balta.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Premium> Premium { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Premium = await _context.Premiums
+                .Include(p => p.Student).ToListAsync();
+        }
+    }
+}
